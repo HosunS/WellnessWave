66847b77101f02464a93236f5518b79a0c032936
alejandro-0502 <gabumon28@gmail.com>
2024-02-03 00:25:45 -0800

    Added interface to LogInView and SignupView as well as navigation between them.

--- WellnessWave/WellnessWave/LogInView.swift
+++ WellnessWave/WellnessWave/LogInView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct LogInView: View {
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color(.blue)
+                    .ignoresSafeArea()
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.white)
+                    .frame(width: 300.0, height: 600.0)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 50.0)
+                VStack {
+                    Text("Wellness Wave").font(.title).fontWeight(.bold).padding(15)
+                    VStack {
+                        TextField("Username", text: $username)
+                        TextField("Password", text: $password)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                    .padding(15)
+                    Button {
+                        LogIn(user: username, pass: password)
+                    } label: {
+                        Text("Login")
+                    }
+                    .padding(.bottom)
+                    HStack {
+                        Text("New to Wellness Wave?")
+                            .font(.caption)
+                            .fontWeight(.light)
+                        NavigationLink {
+                            SignupView()
+                        } label: {
+                            Text("Sign up")
+                                .font(.caption)
+                        }
+                        
+                    }
+                }
+                .frame(width: 300.0, height: 600.0)
+                
+            }
+        }
+        
+    }
+    func LogIn(user:String, pass:String) {
+        print("Your username is \(user)")
+        print("Your password is \(pass)")
     }
 }
 
--- WellnessWave/WellnessWave/SignupView.swift
+++ WellnessWave/WellnessWave/SignupView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.mint)
+                .ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.white)
+                .frame(width: 300.0, height: 600.0)
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal, 50.0)
+            VStack {
+                Text("Wellness Wave").font(.title).fontWeight(.bold).padding(15)
+                VStack {
+                    TextField("Username", text: $username)
+                    TextField("Password", text: $password)
+                }
+                .textFieldStyle(.roundedBorder)
+                .padding(15)
+                Button {
+                    SignUp(user: username, pass: password)
+                } label: {
+                    Text("Sign Up")
+                }
+                .padding(.bottom)
+            }
+            .frame(width: 300.0, height: 600.0)
+            
+        }
+        
+    }
+    func SignUp(user:String, pass:String) {
+        print("Your username is \(user)")
+        print("Your password is \(pass)")
     }
 }
 
