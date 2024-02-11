73f5c34824255062669232aba95b9e999a3cef13
nksongg <nksongg@Gmail.com>
2024-02-10 19:36:55 -0800

    added sleep ui

--- WellnessWave/WellnessWave/SleepView.swift
+++ WellnessWave/WellnessWave/SleepView.swift
@@ -6,12 +6,69 @@
 //
 
 import SwiftUI
+import Foundation
+import UserNotifications
+import UIKit
 
 struct SleepView: View {
+    @State private var bedTime = Date()
+    @State private var wakeTime = Date()
+    @State private var bedTimePicker = false
+    @State private var wakeTimePicker = false
+    
     var body: some View {
-        Text("Hello")
+        VStack {
+            Text("Sleep Stability")
+            // bedtime
+            Image(systemName: "bed.double")
+                .font(.system(size: 50))
+                .padding()
+            
+            Button(action: {
+                bedTimePicker.toggle()
+            }) {
+                Text("Select bedtime")
+            }
+            
+            if bedTimePicker {
+                DatePicker("", selection: $bedTime, displayedComponents: .hourAndMinute)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .padding()
+            }
+            
+            Text("Selected time: \(shortTime(bedTime))")
+                .padding()
+            
+            Image(systemName: "clock")
+                .font(.system(size: 50))
+                .padding()
+            
+            // wakeup
+            Button(action: {
+                wakeTimePicker.toggle()
+            }) {
+                Text("Select wake time")
+            }
+            
+            if wakeTimePicker {
+                DatePicker("", selection: $wakeTime, displayedComponents: .hourAndMinute)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .padding()
+            }
+            
+            Text("Selected time: \(shortTime(wakeTime))")
+                .padding()
+        }
+    }
+    
+    func shortTime(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
     }
 }
+
+
 
 #Preview {
     SleepView()
