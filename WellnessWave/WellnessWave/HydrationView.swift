aa53fc4c463e2e26b95df568e1207cc35047c61e
alejandro-0502 <gabumon28@gmail.com>
2024-02-29 19:13:57 -0800

    Updating Hydration View theme.

--- WellnessWave/WellnessWave/HydrationView.swift
+++ WellnessWave/WellnessWave/HydrationView.swift
@@ -13,76 +13,81 @@ struct HydrationView: View {
     @ObservedObject private var viewModel = HydrationViewModel()
     var body: some View {
         let recommended: Int = Int(viewModel.weight * 0.5)
-        
-        VStack {
-            Text("Recommended amount of water to drink:")
-                .font(.title3)
-                .fontWeight(.medium)
-            Text("\(recommended) oz")
-                .font(.title)
-                .fontWeight(.semibold)
-                .padding(.bottom, 60.0)
-            
-            //Creating Water Meter
-            HStack {
-                Spacer()
-                ZStack(alignment: .bottomLeading) {
-                    
-                    RoundedRectangle(cornerRadius: 15)
-                        .frame(width:200.0, height: viewModel.waterLevel*4)
-                        .foregroundColor(Color(red: 0.4627, green: 0.8392, blue: 1.0))
-                        .animation(Animation.smooth(), value:viewModel.waterLevel)
-                    
-                    RoundedRectangle(cornerRadius: 15)
-                        .frame(width: 200.0, height: 400.0)
-                        .foregroundColor(.blue.opacity(0.1))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(.black, lineWidth: 3))
-                        
-
-                }
-                Spacer()
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.top)
+            VStack {
+                Text("Recommended amount of water to drink:")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.white)
+                Text("\(recommended) oz")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 60.0)
+                    .foregroundStyle(.white)
                 
-                //Oz Buttons and updating viewModel.waterLevel accordingly
-                VStack(spacing: 30) {
-                    Button {
-                        viewModel.waterLevel = min(viewModel.waterLevel + (8.0 * 100.0/CGFloat(recommended)), 100)
-                        viewModel.saveWater(waterLevel: viewModel.waterLevel)
-                    } label: {
-                        Text("8 oz ")
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 40))
+                //Creating Water Meter
+                HStack {
+                    Spacer()
+                    ZStack(alignment: .bottomLeading) {
+                        
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width:200.0, height: viewModel.waterLevel*4)
+                            .foregroundColor(Color(red: 0.4627, green: 0.8392, blue: 1.0))
+                            .animation(Animation.smooth(), value:viewModel.waterLevel)
+                        
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 200.0, height: 400.0)
+                            .foregroundColor(.blue.opacity(0.1))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(.white, lineWidth: 3))
+                        
+                        
                     }
-                    Button {
-                        viewModel.waterLevel = min(viewModel.waterLevel + (16.0 * 100.0/CGFloat(recommended)), 100)
-                        viewModel.saveWater(waterLevel: viewModel.waterLevel)
-                    } label: {
-                        Text("16 oz")
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 40))
+                    Spacer()
+                    
+                    //Oz Buttons and updating viewModel.waterLevel accordingly
+                    VStack(spacing: 30) {
+                        Button {
+                            viewModel.waterLevel = min(viewModel.waterLevel + (8.0 * 100.0/CGFloat(recommended)), 100)
+                            viewModel.saveWater(waterLevel: viewModel.waterLevel)
+                        } label: {
+                            Text("8 oz ")
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 40))
+                        }
+                        Button {
+                            viewModel.waterLevel = min(viewModel.waterLevel + (16.0 * 100.0/CGFloat(recommended)), 100)
+                            viewModel.saveWater(waterLevel: viewModel.waterLevel)
+                        } label: {
+                            Text("16 oz")
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 40))
+                        }
+                        Button {
+                            viewModel.waterLevel = min(viewModel.waterLevel + (32.0 * 100.0/CGFloat(recommended)), 100)
+                            viewModel.saveWater(waterLevel: viewModel.waterLevel)
+                        } label: {
+                            Text("32 oz")
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 40))
+                        }
                     }
-                    Button {
-                        viewModel.waterLevel = min(viewModel.waterLevel + (32.0 * 100.0/CGFloat(recommended)), 100)
-                        viewModel.saveWater(waterLevel: viewModel.waterLevel)
-                    } label: {
-                        Text("32 oz")
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 40))
-                    }
+                    Spacer()
                 }
-                Spacer()
+                Text("\(Int((viewModel.waterLevel * CGFloat(recommended) * 0.01))) oz consumed")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .padding([.top, .leading, .trailing], 20.0)
+                    .foregroundStyle(.white)
             }
-            Text("\(Int((viewModel.waterLevel * CGFloat(recommended) * 0.01))) oz consumed")
-                .font(.title3)
-                .fontWeight(.medium)
-                .padding([.top, .leading, .trailing], 20.0)
         }
         .onAppear {
             viewModel.onAppear()
